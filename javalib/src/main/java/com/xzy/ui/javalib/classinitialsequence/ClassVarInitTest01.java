7fda0320d4f17cfebf9abc7affae3c9146345a9b
hgncxzy <xuzhuyun@fcbox.com>
2021-08-09 23:46:39 +0800

    java类的初始化顺序总结

--- javalib/src/main/java/com/xzy/ui/javalib/classinitialsequence/ClassVarInitTest01.java
+++ javalib/src/main/java/com/xzy/ui/javalib/classinitialsequence/ClassVarInitTest01.java
@@ -2,8 +2,7 @@ package com.xzy.ui.javalib.classinitialsequence;
 
 /**
  *
- * 参考 Java成员变量初始化顺序：
- * https://blog.csdn.net/weixin_38283159/article/details/84454481?utm_medium=distribute.pc_relevant_download.none-task-blog-2~default~BlogCommendFromBaidu~default-1.test_version_3&depth_1-utm_source=distribute.pc_relevant_download.none-task-blog-2~default~BlogCommendFromBaidu~default-1.test_version_
+ * JAVA类初始化顺序总结:https://blog.csdn.net/luoyoub/article/details/82874993
  * 测试类的初始化顺序
  *  结论：类的初始化顺序为：类对象（静态变量）/静态代码块初始化 -->实例变量/非静态代码块初始化-->构造方法初始化
  *       如果有继承关系，先调用父类的构造方法，再调用子类的构造方法。
