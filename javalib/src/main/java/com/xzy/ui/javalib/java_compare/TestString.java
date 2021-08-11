953e223a9dcefe97f1923e360bd5bcf47b60d0e0
hgncxzy <xuzhuyun@fcbox.com>
2021-08-11 10:18:12 +0800

    feature:java对象比较和字符串比较、字符串长度问题

---
+++ javalib/src/main/java/com/xzy/ui/javalib/java_compare/TestString.java
@@ -0,0 +1,41 @@
+package com.xzy.ui.javalib.java_compare;
+
+/**
+ * https://www.cnblogs.com/zhxhdean/archive/2011/03/25/1995431.html
+ *
+ * @author ：created by xzy.
+ * @date ：2021/8/10
+ */
+class TestString {
+    public static void main(String[] args) {
+
+        // 基本数据类型，== 比较的是值，没有equals方法
+        int a = 11;
+        int b = 11;
+        // true
+        System.out.println(a == b);
+        // 编译错误，语法检查不通过
+        // System.out.println(a.equals(b));
+
+        // 复合数据类型（类），如果重写了equals方法，则 == 比较的使对象的引用地址，equals比较的是值
+        // 如果没有重写equals，== 和 equals 比较的都是对象的引用地址
+        String s1 = "xzy";
+        String s2 = "xzy";
+        String s3 = new String("xzy");
+        // true
+        System.out.println(s1 == s2);
+        // false
+        System.out.println(s1 == s3);
+        // true
+        System.out.println(s1.equals(s3));
+        // true
+        System.out.println(s1.equals(s2));
+
+        // 使用字符串缓存池
+        s3 = s3.intern();
+        // true
+        System.out.println(s1 == s3);
+
+
+    }
+}
---
+++ javalib/src/main/java/com/xzy/ui/javalib/string/StringLength.java
@@ -0,0 +1,50 @@
+package com.xzy.ui.javalib.string;
+
+/**
+ * 研究字符串的最大长度问题
+ *
+ * @author ：created by xzy.
+ * @date ：2021/8/10
+ */
+public class StringLength {
+
+    private String s2;
+
+    public static void main(String[] args) {
+        // 正常
+        String s1 = randomStr(65534);
+        System.out.println(s1);
+        System.out.println(s1.length());
+
+        // 正常
+        String s2 = randomStr(65536);
+        System.out.println(s2);
+        System.out.println(s2.length());
+
+        // OOM :Requested array size exceeds VM limit
+        String s3 = randomStr(Integer.MAX_VALUE);
+        System.out.println(s3);
+        System.out.println(s3.length());
+    }
+
+
+    /**
+     * generate random string；
+     */
+    private static String randomStr(int length) {
+        char[] ss = new char[length];
+        int i = 0;
+        while (i < length) {
+            int f = (int) (Math.random() * 3 % 3);
+            if (f == 0) {
+                ss[i] = (char) ('A' + Math.random() * 26);
+            } else if (f == 1) {
+                ss[i] = (char) ('a' + Math.random() * 26);
+            } else {
+                ss[i] = (char) ('0' + Math.random() * 10);
+            }
+            i++;
+        }
+        return new String(ss);
+    }
+}
